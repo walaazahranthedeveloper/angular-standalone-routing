5f340d89d2786ac96b37dc8c3c35053acf2b12f0
walaa_zahran <walaazahran183@gmail.com>
2024-09-21 16:54:10 +0300

    Create Standalone Components

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      login works!
+    </p>
+  `,
+  styles: ``
+})
+export class LoginComponent {
+
+}
